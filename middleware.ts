6db9aaf1b1296a19434d6d5021067b6929db026d
HamsaOmar-dev <hamsaomar21@gmail.com>
2023-05-15 13:54:02 -0500

    added middleare

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+async function apiAccess(req: NextRequest, res: NextResponse) {
+  const authorizedOrigins = [
+    "http://localhost:3000",
+    "https://resolvio-pi.vercel.app",
+  ];
+  if (
+    req.nextUrl.pathname.startsWith("/api") &&
+    !authorizedOrigins.includes(req.nextUrl.origin)
+  ) {
+    return NextResponse.json("Origin Unauthorized");
+  }
+  return NextResponse.next();
+}
+
+export default apiAccess;
